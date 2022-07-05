64148a09937d717f1a5336bc2e1028d1e08dc75a
Kirill Sergeenko <kir_cts@mail.ru>
2022-07-05 23:43:02 +0300

    bugfix 1

--- script.js
+++ script.js
@@ -11,17 +11,6 @@ const nameInput = document.querySelector('#name');
 const jobInput = document.querySelector('#about');
 
 
-function openForm() {
- popupOverlay.classList.add('popup-position_opened');
- stockPopupInputs();
-};
-
-function closeForm(){
-    popupOverlay.classList.remove('popup-position_opened');
-};
-
-openFormButton.addEventListener('click', openForm);
-closeFormButton.addEventListener('click', closeForm);
 
 
 
@@ -36,6 +25,24 @@ function stockPopupInputs() {
 
 
 
+function openForm() {
+ popupOverlay.classList.add('popup-position_opened');
+ stockPopupInputs();
+};
+
+function closeForm(){
+    popupOverlay.classList.remove('popup-position_opened');
+};
+
+openFormButton.addEventListener('click', openForm);
+closeFormButton.addEventListener('click', closeForm);
+
+
+
+
+
+
+
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
@@ -43,12 +50,12 @@ function formSubmitHandler (evt) {
                                                 // О том, как это делать, расскажем позже.
 
     
-    let nameInput = popupForm.querySelector('#name').value;
-    let jobInput =  popupForm.querySelector('#about').value;
-    let userName = document.querySelector('.profile__name');
-    let userJob = document.querySelector('.profile__text');
-    userName.textContent = nameInput;
-    userJob.textContent = jobInput;
+    // let nameInput = popupForm.querySelector('#name').value;
+    // let jobInput =  popupForm.querySelector('#about').value;
+    // userName;
+    // userJob;
+    userName.textContent = nameInput.value;
+    userJob.textContent = jobInput.value;
    // alert(nameInput);
   // closeForm();
 }
