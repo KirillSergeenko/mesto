4fbdbf014cdfbcf61e25e607e6f92d691f6c0985
Kirill Sergeenko <kir_cts@mail.ru>
2023-07-16 00:39:25 +0300

    верстка точно завершена. осталось выяснить попап удаления картинок

--- src/components/card.js
+++ src/components/card.js
@@ -9,13 +9,15 @@ const createCardSelectors = {
     title: '.content-box__title',
     deleteBtn: ".content-box__delete",
     link: ".content-box__photo",
-    contentBox: '.grig-content'
+    contentBox: '.grig-content',
+    likeBox: '.content-box__likebox',
+    likeCounter: '.content-box__like-counter'
 
   };
 
 
 
-const template = document.querySelector(createCardSelectors.template);
+
 
 
 const initialCards = [
@@ -66,7 +68,7 @@ function likeCard(evt){ //лайкосы
     evt.target.classList.toggle('content-box__like_active'); 
   };
   
-  
+const template = document.querySelector(createCardSelectors.template);
   
 function createCard(name, link) { //создает карточку
       const content = template.content.querySelector(createCardSelectors.content).cloneNode(true); 
@@ -77,7 +79,22 @@ function createCard(name, link) { //создает карточку
       contentLink.alt = name;
       content.querySelector(createCardSelectors.deleteBtn).addEventListener('click', () => {content.remove(); }); //для удаления карточки
   
-      content.querySelector(myFormSelectors.likeBtn).addEventListener('click', likeCard); //like
+//добавим в лайккард наш лайкбокс со счетчиком комментов. надо добавить 2 перем
+      const likeBox = content.querySelector(createCardSelectors.likeBox);
+      console.log('likeBox', likeBox);
+
+      // likeBox.querySelector(myFormSelectors.likeBtn).addEventListener('click', likeCard);
+      content.querySelector(myFormSelectors.likeBtn).addEventListener('click', likeCard);
+      console.log('like', content.querySelector(myFormSelectors.likeBtn));
+
+
+      const likeCounter =  likeBox.querySelector(createCardSelectors.likeCounter);
+      console.log('likeCounter', likeCounter);
+
+
+
+// конец зоны экспериментов
+      // content.querySelector(myFormSelectors.likeBtn).addEventListener('click', likeCard); //like было
       
   
       content.querySelector(imageSelectors.openPhotoBtn).addEventListener('click', (evt) => { //opener
@@ -89,7 +106,24 @@ function createCard(name, link) { //создает карточку
       
     };
   
+
+
+    // const createCardSelectors = {
+    //   template: '#card-template',
+    //   content: '.content-box' ,
+    //   title: '.content-box__title',
+    //   deleteBtn: ".content-box__delete",
+    //   link: ".content-box__photo",
+    //   contentBox: '.grig-content'
+    //likeBox: '.content-box__likebox',
+    //likeCounter: '.content-box__like-counter'
+
+    // };
+  
+  
+  
   
 
 
-export {formAddCardSubmitHandler, createCard, createCardSelectors, initialCards};
+export {formAddCardSubmitHandler, createCard, createCardSelectors, initialCards, 
+};
